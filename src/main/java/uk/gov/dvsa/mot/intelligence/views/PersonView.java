ef586c079a12144c917f026bbe6ad986380dfe6f
Szymon Filipczyk <s.filipczyk@kainos.com>
2017-01-23 10:10:00 +0100

    BL-4492 removing freemarker templates

--- src/main/java/uk/gov/dvsa/mot/intelligence/views/PersonView.java
+++ src/main/java/uk/gov/dvsa/mot/intelligence/views/PersonView.java
@@ -6,11 +6,7 @@ public class PersonView extends View {
     private final String person;
 
     public PersonView(String person) {
-<<<<<<< HEAD
-        super("person.ftl");
-=======
         super("person.hbs");
->>>>>>> b31e0e7... BL-4492 replaced free marker with handlebars
         this.person = person;
     }
 
--- src/main/resources/uk/gov/dvsa/mot/intelligence/views/person.ftl
+++
@@ -1,6 +0,0 @@
-<#-- @ftlvariable name="" type="uk.gov.dvsa.mot.intelligence.views.PersonView" -->
-<html>
-    <body>
-        <h1>Hello, ${person?html}!</h1>
-    </body>
-</html>
